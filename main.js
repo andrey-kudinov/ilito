889e426e3d44a28003ab4d55ce6057e5a8ccbeee
Andrey Kudinov <2499931@gmail.com>
2022-05-15 20:31:13 +0400

    Add logic

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log(1)
+const card = document.querySelector('.flip-card')
+card.onclick = () => card.classList.toggle('open')
+
+const countImages = 2
+
+const button = document.querySelector('button')
+
+const openNewCard = () => {
+  const back = document.querySelector('.back-img')
+  back.src = `./images/back-${Math.floor(Math.random() * countImages + 1)}.jpeg`
+  card.classList.add('open')
+}
+
+button.onclick = () => {
+  if (card.classList.contains('open')) {
+    card.classList.remove('open')
+    setTimeout(() => openNewCard(), 800);
+  } else {
+    openNewCard()
+  }
+}
