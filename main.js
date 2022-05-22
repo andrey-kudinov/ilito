2b95b27176484bf6b02cf0cb7a9583e81243fbcc
Andrey Kudinov <2499931@gmail.com>
2022-05-22 16:29:07 +0400

    Update hover effect

--- main.js
+++ main.js
@@ -20,10 +20,10 @@ button.onclick = () => {
   }
 }
 
-button.addEventListener('mousemove', e => {
-  const rect = e.target.getBoundingClientRect()
-  const x = e.clientX - rect.left
-  const y = e.clientY - rect.top
-  button.style.setProperty('--x', x + 'px')
-  button.style.setProperty('--y', y + 'px')
-})
+button.onmousemove = e => {
+  const { left, top } = e.currentTarget.getBoundingClientRect()
+  const x = e.clientX - left
+  const y = e.clientY - top
+  button.style.setProperty('--x', `${x}px`)
+  button.style.setProperty('--y', `${y}px`)
+}
